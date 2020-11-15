f1873f9d1a6351fa1956f0d72c646c6a013ffc49
mustafauyysl <mustafauyysl@gmail.com>
2020-11-15 05:41:51 +0300

    'rename-android'

--- ios/formlaynTests/formlaynTests.m
+++ ios/formlaynTests/formlaynTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface formlaynTests : XCTestCase
+@interface FormlaynTests : XCTestCase
 
 @end
 
-@implementation formlaynTests
+@implementation FormlaynTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
