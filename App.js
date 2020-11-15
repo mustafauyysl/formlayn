d26be3f9d5f5f23382ad3c2c20ac2c3f116423d0
Mustafa <mustafa@Mustafa-Mac.local>
2020-11-15 05:45:18 -0800

    icon-splashscren

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import configureStore from './redux/reducers/configureStore';
 import MainNavigator from './navigation/MainNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 const store = configureStore();
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  })
   return(
     <Provider store={store}>
        <MainNavigator />
